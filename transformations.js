bb7c65ca0d5cf1d7b47ee12ae60a57e9efedf712
kelvinchow2003 <112141495+kelvinchow2003@users.noreply.github.com>
2023-05-25 16:18:07 -0400

    Add files via upload

--- transformations.js
+++ transformations.js
@@ -11,14 +11,45 @@ function isElementInViewport(element) {
   function handleScroll() {
     var rectangle = document.querySelector('.rectangle');
     var header2 = document.querySelector('.header2');
+    
+    
   
     if (isElementInViewport(rectangle)) {
       rectangle.classList.add('visible');
       header2.classList.add('visible');
+    
     } else {
       rectangle.classList.remove('visible');
       header2.classList.remove('visible');
+    
     }
   }
+  function open_about() {
+    var section = document.getElementById('About');
+    section.scrollIntoView({ behavior: 'smooth' });
+    console.log('Button clicked!');
+  }
+  function open_main() {
+    var section = document.getElementById('Main');
+    section.scrollIntoView({ behavior: 'smooth' });
+    console.log('Button clicked!');
+  }
+  
   
   window.addEventListener('scroll', handleScroll);
+
+  const fish = document.querySelector('.fish');
+
+  function getRandomValue(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  
+  function animateFish() {
+    const x = getRandomValue(-100, 100) + 'px';
+    const y = getRandomValue(-50, 50) + 'px';
+  
+    fish.style.setProperty('--x', x);
+    fish.style.setProperty('--y', y);
+  }
+  
+  setInterval(animateFish, 2000);
